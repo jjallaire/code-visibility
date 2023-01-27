501ea520460a73bd9271df46a3be56b347ab2cca
shafayetShafee <seaborn.sicario@gmail.com>
2023-01-27 22:19:21 +0600

    added comment-directive option

--- _extensions/code-visibility/code-visibility.lua
+++ _extensions/code-visibility/code-visibility.lua
@@ -1,10 +1,51 @@
+local str = pandoc.utils.stringify
+-- local p = quarto.log.output
+
 -- remove any lines with the hide_line directive.
 function CodeBlock(el)
   if el.classes:includes('cell-code') then
     el.text = filter_lines(el.text, function(line)
-      return not (line:match("#| ?hide_line%s*$") or line:match("#>"))
+      return not line:match("#| ?hide_line%s*$")
     end)
     return el
+  end
+end
+
+-- function for applying comment_directive
+local function apply_cmnt_directives(comment_directive)
+  local cmnt_directive_pattern = "^" .. comment_directive
+  local line_filter = {
+    CodeBlock = function(cb)
+      if cb.classes:includes('cell-code') then
+        cb.text = filter_lines(cb.text, function(line)
+            return not line:match(cmnt_directive_pattern)
+            end)
+        return cb
+      end
+    end
+  }
+  return line_filter
+end
+
+-- function to check whether a value exists in a table
+function has_value(table, value)
+  for k, v in ipairs(table) do
+    if v == value then
+      return true
+    end
+  end
+  return false
+end
+
+-- hide lines with comment directive
+function Pandoc(doc)
+  local meta = doc.meta
+  local cmnt_directive_tbl = {"#>", "//>"}
+  if meta['comment-directive'] then
+    if has_value(cmnt_directive_tbl, str(meta['comment-directive'])) then
+      local comment_directive = str(meta['comment-directive'])
+      return doc:walk(apply_cmnt_directives(comment_directive))
+    end
   end
 end
 
